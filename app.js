d876cfb157c5d0364ec2f15e80c2eb3cb660b1cd
zwinkel15 <carlos.pelinco.h@uni.pe>
2025-03-18 19:40:57 -0500

    inicializando repositorio

---
+++ app.js
@@ -0,0 +1,24 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const inputAmigo=document.getElementById("amigo");
+const listaAmigos=[];
+const ulListaAmigos= document.getElementById("listaAmigos");
+const ulResultado=document.getElementById("resultado");
+function agregarAmigo(){
+    if(!inputAmigo.value){
+        alert("input lleno");
+
+    }else{
+        alert("input sin informacion");
+    }
+    listaAmigos.push(inputAmigo.value);
+    ulListaAmigos.innerHTML+=`<li>${inputAmigo.value}</li>`;
+
+}
+
+function sortearAmigo(){
+    const random=Math.floor(Math.random()*listaAmigos.length);
+    const amigoSecreto= listaAmigos[random];
+    ulResultado.innerHTML=`<li>el amigo secreto es: ${amigoSecreto}</li>`;
+
+    
+}
